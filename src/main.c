6f69b148c9f012540bd064b312c2bbfb5f301c7c
FBIAgent05 <74946397+FBIAgent05@users.noreply.github.com>
2024-05-02 08:20:27 +0000

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,28 @@
+                                                    /*** Chess: ***/
+                                                /*** By: FBIAgent05 ***/
+                                                /* 開始: Feb 20, 2024*/
+                                                /* 終了: Apr, 20, 2024*/
+                                                /* 再改修: May, 02, 2024*/
+
+
+#include "../header/chess_lib.h"
+
+int main(void)
+{
+    int board[S_BOARD][S_BOARD];
+    initBoard(board);
+
+    clear();
+    welcome();
+
+
+    while (gameinProgress())
+    {
+        clear();
+
+        printBoard(board);
+        moveBoard(board);
+    }
+
+    return 0;
+}
