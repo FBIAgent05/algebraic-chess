ed79116a6de60fd4f682ad1ad13bf14ebcaefd9a
千歳 <74946397+FBIAgent05@users.noreply.github.com>
2024-05-03 09:55:15 +0000

    create separate header for user interface

---
+++ src/UI.h
@@ -0,0 +1,15 @@
+#ifndef UI_H
+#define UI_H
+
+#include "chess_lib.h"
+
+#define S_BOARD 8
+
+void printBoard (int board[S_BOARD][S_BOARD]);
+
+bool displayASCII(void);
+bool useASCII (bool x);
+
+
+
+#endif
